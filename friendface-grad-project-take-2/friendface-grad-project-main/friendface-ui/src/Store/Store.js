7a3def2a0512c010e03253ecfbea14a6c68f9495
WillMarriott <wmarriott@scottlogic.com>
2023-10-04 10:55:33 +0100

    Set project up for Maven.

--- friendface-grad-project-take-2/friendface-grad-project-main/friendface-ui/src/Store/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import postsSlice from "./Features/postsSlice"
-
-export const store = configureStore({
-    reducer: {
-        posts: postsSlice,
-        
-    },
-})
