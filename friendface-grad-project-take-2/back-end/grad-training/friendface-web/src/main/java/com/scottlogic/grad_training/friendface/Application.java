f43864d330438fc7df94a67b42f8e78ce52f6519
WillMarriott <wmarriott@scottlogic.com>
2023-10-10 09:13:12 +0100

    Completed the Spring section of the repo

--- friendface-grad-project-take-2/back-end/grad-training/friendface-web/src/main/java/com/scottlogic/grad_training/friendface/Application.java
+++ friendface-grad-project-take-2/back-end/grad-training/friendface-web/src/main/java/com/scottlogic/grad_training/friendface/Application.java
@@ -2,11 +2,14 @@ package com.scottlogic.grad_training.friendface;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
+
     }
+
 }
--- friendface-grad-project-take-2/back-end/grad-training/friendface-web/src/main/java/com/scottlogic/grad_training/friendface/GreetingController.java
+++ friendface-grad-project-take-2/back-end/grad-training/friendface-web/src/main/java/com/scottlogic/grad_training/friendface/GreetingController.java
@@ -1,13 +1,40 @@
 package com.scottlogic.grad_training.friendface;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/greetings")
 public class GreetingController {
 
     @GetMapping
     public String index() {
         return "Sup";
     }
+
+    @GetMapping("/{locale}")
+    public String localized(@PathVariable String locale) {
+        if (locale.equals("fr")) {
+            return "Bonjour";
+        }
+        return "Hello";
+    }
+
+    @GetMapping("/employee")
+    public Employee employeeOfTheMonth() {
+        return employeeOfTheMonthService.computeEmployeeOfTheMonth();
+    }
+
+
+    @PostMapping("/welcome")
+    public String welcome(@RequestBody Employee employee) {
+        return String.format("Welcome %s!", employee.getName());
+    }
+
+    private final EmployeeOfTheMonthService employeeOfTheMonthService;
+
+    public GreetingController(EmployeeOfTheMonthService employeeOfTheMonthService) {
+        this.employeeOfTheMonthService = employeeOfTheMonthService;
+    }
+
+
 }
